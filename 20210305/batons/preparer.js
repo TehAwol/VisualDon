49309a81db4049889c8d56d1421978fe0e51135f
Mikkel Vestergaard <56039404+vestermikk@users.noreply.github.com>
2021-03-11 18:02:36 +0100

    Sorting algorithm and implementation

--- 20210305/batons/preparer.js
+++ 20210305/batons/preparer.js
@@ -1,18 +1,35 @@
+const { sort } = require('d3-array');
 const data = require('./villes.json');
 
-// const find = ville => ville["Canton"] && 
-// ville["Scenario1_RoofsOnly_PotentialSolarElectricity_GWh"];
-
-var groupBy = function(xs, key) {
-    return xs.reduce(function(rv, x) {
-      (rv[x[key]] = rv[x[key]] || []).push(x);
-      return rv;
-    }, {});
-  };
-
 const resultat = data
-.map(ville => ({canton: ville["Canton"], power: ville["Scenario1_RoofsOnly_PotentialSolarElectricity_GWh"]}))
+  .map(ville => ({ canton: ville["Canton"], power: ville["Scenario1_RoofsOnly_PotentialSolarElectricity_GWh"] }));
 
-const resultat2 = groupBy(resultat, 'canton').forEach(canton => canton.reduce(acc, cv))
+let holder = [];
+let i = 0;
 
+resultat.sort();
+
+resultat.forEach((res) => {
+  if (holder.length == 0) {
+    holder.push(res);
+  }
+  if (holder[i].canton === res.canton) {
+    holder[i].power += res.power;
+  } else {
+    i++;
+    holder.push(res);
+  }
+});
+
+i = 0;
+
+holder.forEach((res) => {
+  if (res.canton === undefined || res.power === undefined) {
+    holder.splice(i, 1)
+  } else {
+    i++;
+  }
+})
+
+console.log(JSON.stringify(holder));
 
--- 20210305/data.md
+++ 20210305/data.md
@@ -1,7 +1,9 @@
 # Exercice 3 data.md
 1. Téléchargement des données en format .js
-2. Création du fichier preparer.js qui enlève les informations non nécessaire et additionne les valeurs d'énergies par Cantons
-```javascript 
-const resultat = data
-.map(ville => ({canton: ville["Canton"], power: ville["Scenario1_RoofsOnly_PotentialSolarElectricity_GWh"]}))
+2. Création du fichier preparer.js qui enlève les informations non nécessaire et additionne les valeurs d'énergies par Cantons respectifs
+
+Pour enlever les données non utilisées pour le graphes
+
+```javascript
+
 ```
