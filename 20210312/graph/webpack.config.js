4b3318252dc4d3adde36dd75c28ea016b6f3f421
Mikkel Vestergaard <vestergaard.mikk@gmail.com>
2021-04-13 19:57:39 +0200

    Bundler switched to Webpack
    Bundler switched to Webpack-Babel

---
+++ 20210312/graph/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: false,
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(handlebars|hbs)$/,
+        use: [{
+            loader: 'handlebars-loader',
+            options: {
+                helperDirs: path.resolve('./src/templates/helpers')
+            }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ]
+  },
+};
