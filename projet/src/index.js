6657e5c669fa32fff5d0c96173091941f8ca559e
Mikkel Vestergaard <56039404+vestermikk@users.noreply.github.com>
2021-06-19 15:37:53 +0200

    Updated dataset

--- projet/src/index.js
+++ projet/src/index.js
@@ -1,39 +1,141 @@
+var $ = require('jquery');
 var countries = require('countries-earnings.geo.json');
-var data = require('dataset.json');
+const {
+    map
+} = require('jquery');
 
 var myMap = L.map('map').setView([28, 0], 2);
+var info = L.control();
 
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+L.tileLayer('', {
+    // attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     minZoom: 0,
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
 }).addTo(myMap);
 
-L.geoJSON(countries).addTo(myMap);
+var countries = L.geoJson(countries, {
+    style: styleEarnings,
+    onEachFeature: onEachFeature
+}).addTo(myMap);
 
-
-function getColor(d) {
-    return d > 40000000 ? '#800026' :
-           d > 10000000 ? '#800026' :
-           d > 7500000  ? '#BD0026' :
-           d > 5000000 ? '#E31A1C' :
-           d > 2500000  ? '#FC4E2A' :
-           d > 1000000   ? '#FD8D3C' :
-           d > 500000   ? '#FEB24C' :
-           d > 100000   ? '#FED976' :
-                      '#FFEDA0';
+function getColorEarnings(e) {
+    return e > 40000000 ? '#99000d' :
+        e > 10000000 ? '#cb181d' :
+        e > 7500000 ? '#ef3b2c' :
+        e > 5000000 ? '#fb6a4a' :
+        e > 2500000 ? '#fc9272' :
+        e > 1000000 ? '#fcbba1' :
+        e > 500000 ? '#fee5d9' :
+        e > 100000 ? '#fff5f0' :
+        'white';
 }
 
-function style(feature) {
+function getColorEarningsPop(e) {
+    return e > 5 ? '#99000d' :
+        e > 4.5 ? '#cb181d' :
+        e > 3.5 ? '#ef3b2c' :
+        e > 2.5 ? '#fb6a4a' :
+        e > 1.5 ? '#fc9272' :
+        e > 0.5 ? '#fcbba1' :
+        e > 0.001 ? '#fee5d9' :
+        'white';
+};
+
+
+function styleEarnings(feature) {
     return {
-        fillColor: getColor(feature.properties.earnings),
-        weight: 2,
+        fillColor: getColorEarnings(feature.properties.earnings),
+        weight: 1,
         opacity: 1,
+        fillOpacity: 0.7,
         color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
     };
 }
 
-L.geoJson(countries, {style: style}).addTo(myMap);
+function styleEarningsPop(feature) {
+    return {
+        fillColor: getColorEarningsPop(feature.properties.avgEarnings),
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.7,
+        color: 'white',
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 1,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var mapMode = "earnings";
+
+function resetHighlight(e) {
+    if (mapMode === "earnings") countries.setStyle(styleEarnings);
+    if (mapMode === "avgEarnings") countries.setStyle(styleEarningsPop);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    myMap.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    if (mapMode === "earnings") {
+        this._div.innerHTML = '<h4>Total Earnings</h4>' + (props ?
+            '<b>' + props.ADMIN + '</b><br />' + (props.earnings ? formatter.format(props.earnings) + ' earned total' : 'No data available') :
+            'Hover over a country');
+    } else if (mapMode === "avgEarnings") {
+        this._div.innerHTML = '<h4>Earnings per Capita</h4>' +  (props ?
+            '<b>' + props.ADMIN + '</b><br />' + (props.avgEarnings ? formatter.format(props.avgEarnings) + ' per capita' : 'No data available')
+            : 'Hover over a country');
+    }
+
+};
+
+info.addTo(myMap);
+
+$('#btn-earnings').on('click', () => {
+    mapMode = "earnings";
+    countries.setStyle(styleEarnings);
+    info.update();
+})
+
+$('#btn-earnings-avg').on('click', () => {
+    mapMode = "avgEarnings";
+    countries.setStyle(styleEarningsPop);
+    info.update();
+
+})
