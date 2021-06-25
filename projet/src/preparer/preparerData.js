fcaedcbd22f96ebe106fce0f96149f4c16dedd95
Martin Mertenat <41158878+Grolims@users.noreply.github.com>
2021-06-25 14:33:56 +0200

    preparer

---
+++ projet/src/preparer/preparerData.js
@@ -0,0 +1,95 @@
+
+const data2 = require('./barrace.json');
+const fs = require('fs');
+
+
+// plus utiliser
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const result = data2
+    .map(d => ({ date: d.date, nom: d.jeu, totGain: d.totaldesGains}))
+   .sort((a, b) => a.date > b.date ? -1 : 1);
+
+  let annee = [];
+  let names = [];
+  let earnings = [];
+  let player =[];
+
+  result.forEach((e) => {
+       
+       if (!annee.includes(e.date)) annee.push(e.date);
+   });
+
+   result.forEach((e) => {
+     
+        if (!names.includes(e.nom)) names.push(e.nom);
+    });
+
+    // mise en place nom et date et total gain
+
+    names.forEach(n => {
+
+      annee.forEach(element => {
+
+        let total = 0;
+        let color =0;
+
+        result.forEach(e => {
+          if (n === e.nom && element === e.date) {
+            total += parseInt(e.totGain);
+
+          }
+      })
+  
+      earnings.push({ Name: n, Earnings: total, Annee: element, });
+  
+        
+      });
+
+  });
+
+
+  function recupAnne (Anne)
+  {
+    return earnings.filter(d=>d.Annee === Anne && d.Earnings >= 1000).sort((a, b) => a.Earnings > b.Earnings ? -1 : 1)
+    .filter((d,i)=> i < 6);
+  }
+
+
+const toutAnnee = Array.from(new Set(earnings.map(d => d.Annee))).sort();
+//iteration pour chaque annee
+const resultat = toutAnnee.reduce( (r,d) => ({...r,[d]:recupAnne(d)}),({}));
+
+
+
+// total par anne
+let gainAnne = [];
+annee.forEach(element => {
+
+  let total = 0;
+
+  result.forEach(e => {
+    if (element === e.date) total += parseInt(e.totGain);
+})
+
+gainAnne.push({Earnings: total, Annee: element });
+
+  
+});
+
+
+  //top 5 cashprize jeux par annee 
+ fs.writeFileSync("JeuxParAnne",JSON.stringify(resultat,null,4), "utf-8")
+
+ //tot par annee
+ //const filterAnne= gainAnne.sort((a, b) => a.Anne > b.Anne ? 1 : -1);
+
+// total cashprize par anne
+//fs.writeFileSync("TotParAnne",JSON.stringify(filterAnne,null,3), "utf-8")
---
+++ projet/src/preparer/toJSON.js
@@ -0,0 +1,24 @@
+//convertir les données CSV en JSON
+
+const fs = require('fs')
+const d3 = require('d3')
+
+const file2 = fs.readFileSync('HistoricalEsportData.csv', 'utf-8')
+
+const { parse } = d3.dsvFormat(',')
+
+
+
+const data2 =(
+  parse(file2)
+
+    .map(d => ({
+      date:d.Date.substring(5),
+      jeu: d.Game,
+      totaldesGains:d.Earnings,
+      player:d.Players,
+    }))
+)
+
+
+fs.writeFileSync("barrace.json",JSON.stringify(data2,null,2), "utf-8")
